a6abd9a6374884a56326284edef6aec479be8c75
Nishant Deshmukh <nishantd369@gmail.com>
2024-01-14 19:45:40 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Home from './pages/home';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Project from './pages/Project';
