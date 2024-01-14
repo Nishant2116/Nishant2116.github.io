c7f3d37674a0a9f844ea897b43a4578c020f6c87
Nishant Deshmukh <nishantd369@gmail.com>
2024-01-14 19:25:18 +0530

    Initial commit

--- src/assets/project/index.js
+++ src/assets/project/index.js
@@ -1,13 +1,19 @@
 import project_1 from './project_1.png';
 import project_2 from './project_2.png';
 import project_3 from './project_3.png';
+import project_4 from './project_4.png';
 import vid_1 from './project_1_vid.mp4';
 import vid_2 from './project_2_vid.mp4';
+import vid_3 from './project_3_vid.mp4';
+import vid_4 from './project_4_vid.mp4';
 
 export {
     project_1,
     project_2,
     project_3,
+    project_4,
     vid_1,
-    vid_2
+    vid_2,
+    vid_3,
+    vid_4
 }
--- src/constant/projectData.js
+++ src/constant/projectData.js
@@ -1,4 +1,4 @@
-import { project_1, project_2, project_3, vid_1 ,vid_2 } from "../assets/project"
+import { project_1, project_2, project_3,project_4, vid_1 ,vid_2,vid_3,vid_4 } from "../assets/project"
 
 
 export const projects = [
@@ -15,7 +15,7 @@ export const projects = [
   
       link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
   
-      color:"#7A5240"
+      color:"#7C4732"
   
     },
   
@@ -31,23 +31,37 @@ export const projects = [
   
       link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
     
-      color:"#5B9DFE"
+      color:"#0D6EFD"
   
     },
   
+    {
+  
+      title: "Olienda",
+  
+      description: "Estisource—an app for precise cost estimates of mechanical parts. Simplifying the complexity, it's your go-to for accurate figures.",
+  
+      src: project_3,
+
+      vid_src:vid_3,
+  
+      link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
+  
+       color:"#FF9F0D"
+    },
     {
   
       title: "Apexion",
   
       description: "Estisource—an app for precise cost estimates of mechanical parts. Simplifying the complexity, it's your go-to for accurate figures.",
   
-      src: project_3,
+      src: project_4,
 
-      vid_src:"",
+      vid_src:vid_4,
   
       link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
   
-       color:"#C49906"
+       color:"#195A00"
     },
   
   
